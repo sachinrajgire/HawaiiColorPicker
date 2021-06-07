e00ed43ec7c781c220160da0e3b4e174fb696e84
sachinrajgire <ketakiharpale@Ketakis-MacBook-Pro-2.local>
2021-06-07 04:42:14 -0400

    Wynisco Commit

---
+++ myjs.js
@@ -0,0 +1,20 @@
+
+function decode(color1, color2) {
+
+
+    let response = fetch('https://ew-color-mixer.herokuapp.com/colors/:lchelani@umd.edu')
+      .then(response => response.json())
+      .then(data => {
+      
+    
+      
+          })
+      
+      }
+
+    
+    
+    decode ()
+    
+    
+    
